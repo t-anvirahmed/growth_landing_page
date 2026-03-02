cee455473e4addc30c9d5bc891e1d597a3391783
t-anvirahmed <t.anvirahmed@outlook.com>
2026-03-03 00:54:49 +0800

    added responsive styles

--- js/main.js
+++ js/main.js
@@ -33,3 +33,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// Mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => 
+        mobileMenu.classList.toggle('active')
+    );
+});
